375cc6cd8e1d1a976ca271ba1cf674633851de97
James Cai <jamescai@genomezoo.net>
2021-07-19 17:02:04 -0500

    Update test_qft.m

--- test_qft.m
+++ test_qft.m
@@ -1,3 +1,7 @@
+g=Gates;
+% https://quantumcomputing.stackexchange.com/questions/15839/where-is-cz-gate-in-qiskit-circuit-composer
+norm(H*X*H - Z)
+
 norm(kron(I,H)*CX*kron(I,H) - CZ)
 
 % https://www.youtube.com/watch?v=rRblvBZkz7A
