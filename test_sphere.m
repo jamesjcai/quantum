258f8cb78b9e40214a2f53b2585c5ba9d21a0114
James Cai <jamescai@genomezoo.net>
2021-10-24 09:47:00 -0500

    Update test_sphere.m

--- test_sphere.m
+++ test_sphere.m
@@ -9,31 +9,35 @@ ket0=[1;0];
 ket1=[0;1];
 
 plotBlochShphere
+ket2bv(ket0)
+ket2bv(ket1)
 plotBlochVect(H*ket0)
 
 function rho = ket2dm(ket)
-% convert ket to a density matrix rho
-rho=ket*ket';
+    % convert ket to a density matrix rho
+    rho=ket*ket';
 end
 
 function lambda = ket2bv(ket)
-rho=ket2dm(ket);
-X=[0 1; 1 0]; Z=[1 0; 0 -1];
-Y=1i*X*Z;
-lambda=[trace(X*rho); trace(Y*rho);trace(Z*rho)];
+    rho=ket2dm(ket);
+    X=[0 1; 1 0]; Z=[1 0; 0 -1];
+    Y=1i*X*Z;
+    lambda=[trace(X*rho); trace(Y*rho); trace(Z*rho)];
 end
 
 function plotBlochVect(ket)
-lambda =ket2bv(ket);
-someBV=line([0 lambda(1)],[0 lambda(2)],[0 lambda(3)],...
-    'marker','o'); 
+    lambda =ket2bv(ket);
+    someBV=line([0 lambda(1)],[0 lambda(2)],[0 lambda(3)],...
+        'marker','o','linewidth',2); 
 end
 
 function plotBlochShphere
-[X,Y,Z] = sphere;
-f=surf(X,Y,Z);
+[X,Y,Z] = sphere(50);
+f=surf(X,Y,Z,'FaceColor',[.5 .5 .5],...
+    'LineStyle','none','EdgeColor','none');
 axis equal
-shading interp
+% shading interp
+% colormap gray    
 f.FaceAlpha = 0.25;
 line([-1 1], [0 0], [0 0],'color','k')
 line([0 0],[-1 1],[0 0],'color','k')
@@ -42,5 +46,7 @@ text(0,0,1.1,"$\left| 0 \right>$",'Interpreter','latex')
 text(1.1,0,0,"$\left| + \right>$",'Interpreter','latex')
 text(-1.1,0,0,"$\left| - \right>$",'Interpreter','latex')
 text(0,0,-1.1,"$\left| 1 \right>$",'Interpreter','latex')
+text(0,-1.1,0,"$\left| -i \right>$",'Interpreter','latex')
+text(0,1.1,0,"$\left| i \right>$",'Interpreter','latex')
 view([60 15])
 end
